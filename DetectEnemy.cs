faba668fbdb0da335f3777406868b96d3734738c
BrianRMoo <69164107+BrianRMoo@users.noreply.github.com>
2022-04-27 22:29:05 -0500

    Update DetectEnemy.cs

--- DetectEnemy.cs
+++ DetectEnemy.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectEnemy : MonoBehaviour {
-
+    //get the object(player) 
     public GameObject target;
 
+    //create a zone that the enemy ships have to detect the player object
     private void OnTriggerStay(Collider other)
-    {
+    {        
         if(other.gameObject.CompareTag("Target"))
         {
             target.SetActive(true);
         }
     }
 
+    //detec whether the player is in range or not
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.CompareTag("Target"))
