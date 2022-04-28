9f6396c604fcec78fe1be78dde9f316091c677b9
BrianRMoo <69164107+BrianRMoo@users.noreply.github.com>
2022-04-27 22:32:59 -0500

    Update EnlargeLocalScale.cs

--- EnlargeLocalScale.cs
+++ EnlargeLocalScale.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class EnlargeLocalScale : MonoBehaviour {
 
     public float increaseAmount;
-	
-	void Start () {
-
-        
-	}
+	//for use with the background objects
 	void Update () {
         //Takes the given objects scale and multiplies it by a Vector of ones * a seconds of increased amount
         transform.localScale += Vector3.one * Time.deltaTime * increaseAmount;
